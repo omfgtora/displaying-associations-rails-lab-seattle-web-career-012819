747f1a21ec507c596c27b490b1491857f49a23f9
omfgtora <spaminol@gmail.com>
2019-02-26 18:08:06 -0800

    Done.

--- db/migrate/20160108015311_create_songs.rb
+++ db/migrate/20160108015311_create_songs.rb
@@ -2,7 +2,8 @@ class CreateSongs < ActiveRecord::Migration
   def change
     create_table :songs do |t|
       t.string :title
-
+      t.references :artist
+      
       t.timestamps null: false
     end
   end
